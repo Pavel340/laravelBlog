eb21cdc5186e8f60d0a1dbb6869f7c5152e6ea1f
Pavel340 <zinovev-pasha@mail.ru>
2017-10-16 12:14:37 +0300

    Пробный простой блог на Laravel

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Post;
+
+class PagesController extends Controller
+{
+    public function index() {
+        
+        $categories = Category::all();
+        return view('face.index', ['categories' => $categories]);
+    }
+    
+    public function category($categorySlug) {
+        $category = Category::where('slug', $categorySlug)->first();
+        return view('face.category', ['category' => $category]);
+    }
+    
+    public function post($categorySlug, $postSlug) {
+        $category = Category::where('slug', $categorySlug)->first();
+        $post = Post::where('slug', $postSlug)->first();
+        if ($category->exists()
+            && $post->exists()
+            && $post->category_id == $category->id) {
+            return view('face.post', compact('post'));
+        }
+        abort(404);
+    }
+}
---
+++ resources/views/face/index.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('title', 'Главная страница')
+
+@section('content')
+	@foreach($categories as $category)
+		<a href="{{ route('category', $category->slug) }}">
+			{{ $category->title }}
+		</a>
+	@endforeach
+@endsection
